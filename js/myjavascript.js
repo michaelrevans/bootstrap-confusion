e9bbae15aa9f00729237108c442eff7ae419acb6
Michael Evans <michealevans@Michael-Evans-MacBook-Pro.local>
2016-02-04 22:47:31 +0100

    First commit

---
+++ js/myjavascript.js
@@ -0,0 +1,17 @@
+/*
+$(document).ready(function() {
+	$('[name="date"]').hover(function() {
+		$('#date-glyph').hide();
+	}, function() {
+		$('#date-glyph').show();
+	});
+});
+
+$(document).ready(function() {
+	$('[name="time"]').hover(function() {
+		$('#time-glyph').hide();
+	}, function() {
+		$('#time-glyph').show();
+	});
+});
+*/
